9c584409d281ee22c742f8590680dad75b683ae0
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-04-12 14:46:20 +0900

    Codi_crawling.py
    배열을 추가하여 정보를 저장할 공간을 만듦.
    가독성을 위해 띄어쓰기를 포함하여 저장.

--- codi_crawling.py
+++ codi_crawling.py
@@ -11,9 +11,22 @@ driver.implicitly_wait(3)
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+title_list = []
+codi_list = []
+
 items = soup.find('section', class_='Coordi_coordi__3fqAN').find_all('div', class_='CoordiCard_card__1pMHY')
 for i, item in enumerate(items):
     title = item.find('div', class_='CoordiCard_title__xUWCe').get_text(strip=True)
     image_url = item.find('picture', class_='CoordiCard_picture__1N8ub').find('img')['src']
     
     urllib.request.urlretrieve(image_url, '{}.png'.format(i+1))
+
+     codi = ''
+    temp = item.find('div', class_='CoordiCard_tags__2XVPv').find_all('a')
+    for a in temp:
+        codi += a.get_text(strip=True) + ', '
+    codi = codi[:-2]
+    title_list.append(title)
+    codi_list.append(codi)
+    
+    
