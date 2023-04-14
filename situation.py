fc4f75c47d47967fe14d04d73797be5f067448bf
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-04-14 14:15:39 +0900

    엑셀에 있는 여러가지 코디가 추천되는 현상 발생
    우리가 원하는 프로그램의 방식과 다른 구조의 출력.
    때문에 반복문과 조건문을 추가하여 랜덤으로 한 가지의 코디만 추천하도록 수정함.

--- situation.py
+++ situation.py
@@ -12,21 +12,34 @@ current_situation = input("어떤 상황에서 입어야 하는 옷인지를 알
 def get_similar_words(word, words_list):
     return get_close_matches(word, words_list, n=1, cutoff=0.8)
 
-# 입력받은 정보와 유사한 데이터 추출
-filtered_df = df[df['codi'].str.contains('|'.join(get_similar_words(current_situation, df['codi'])))]
-filtered_df = filtered_df.reset_index(drop=True)
+while True:
+    # 입력받은 정보와 유사한 데이터 추출
+    #'|' (or연산)으로 유사한 단어들을 문자열로 변환 -> codi 열에 이 패턴이 포함되는지 확인 
+    # 유사한 단어가 하나라도 포함되어 있다면 filtered_df에 저장 
+    filtered_df = df[df['codi'].str.contains('|'.join(get_similar_words(current_situation, df['codi'])))]
+    filtered_df = filtered_df.reset_index(drop=True)
 
-# 추천 받을 데이터를 배열에 저장
-recommended_outfits = filtered_df['title'].tolist()
+    # 필터링된 추천 받을 데이터를 배열에 저장
+    recommended_outfits = filtered_df['title'].tolist()
 
-# 추천할 옷 랜덤 선택
-if recommended_outfits:
-    random_outfit = random.choice(recommended_outfits)
-    print("추천되는 코디:")
-    print(random_outfit)
-else:
-    print("일치하는 코디가 없습니다.")
+    # 추천할 옷 랜덤 선택
+    # 상황에 알맞은 코디를 하나만 추천하기 위하여 랜덤으로 하나만 선택. 
+    if recommended_outfits:
+        random_outfit = random.choice(recommended_outfits)
+        print("추천되는 코디:")
+        print(random_outfit)
+    else:
+        print("일치하는 코디가 없습니다.")
+        break
 
-# 입력받은 정보 출력하기
-print(current_situation +" 상황에 입기 좋은 옷을 추천해보았습니다!")
+    # 사용자의 답변 받기
+    answer = input("추천된 코디가 마음에 드나요? (예/아니오): ")
+    if answer.lower() == '예':
+        print("좋아요! 코디가 마음에 드셨다니 다행입니다!")
+        break
+    elif answer.lower() == '아니오':
+        print("다른 코디를 추천해드릴게요!")
+        continue
+    else:
+        print("예 또는 아니오로 대답해주세요.")
 
