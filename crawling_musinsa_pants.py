78b95e3bf1e43eba4490d676ebe2b93d18878e7b
wan-su-2020 <poj5928@gmail.com>
2023-04-29 17:26:51 +0900

    오류 수정 및 엑셀 파일에 타이틀 저장 추가

--- crawling_musinsa_pants.py
+++ crawling_musinsa_pants.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import urllib.request
+import pandas as pd
 import time
 
 url = 'https://www.musinsa.com/categories/item/003' #해당 URL갖고옴
@@ -16,5 +17,16 @@ items = soup.find('ul', id='searchList').find_all('li', class_='li_box')
 for i, item in enumerate(items):
     title = item.find('div', class_='article_info').find('p', class_='list_info').find('a').get_text(strip=True)#타이틀 찾아오기
     image_url = 'https:' + item.find('img', class_='lazyload')['data-original']#이미지url갖고옴
+title_list.append(title)
 
-    urllib.request.urlretrieve(image_url, '{}.png'.format(title)) #url이용해서 이미지를 다운받음
+if'/' in title:
+    title=title.relplace('/', '-')# title에 '/'가 들어간 옷 때문에 오류 발생 '-'로 대체
+
+    urllib.request.urlretrieve(image_url, 'img/{}.png'.format(title)) #url이용해서 이미지를 다운받음
+
+    print(title)
+    print(image_url)
+
+data = {'title': title_list} #엑셀파일에 하의 타이틀 저장
+df=pd.DataFrame(data)
+df.to_excel("pants.xlsx")
