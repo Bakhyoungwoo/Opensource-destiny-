c40aae523d83d462c0a5abc3a24647dae42dc649
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-05 13:23:01 +0900

    이미 추천한 것은 더이상 추천하지 않도록 수정 완료
    추천된 코디를 저장하는 리스트롤 따로 만듬.
    uinque_outfits를 리스트로 따로 또 만듬.
    
    이를 통해 두 리스트로 비교를 하여 이미 추천된 코디는 추천 리스트에서 배제함.
    마지막까지 사용자에게 추천해줄 코디가 남아 있지 않다면 "더 이상 추천할 코디가 없습니다"를 출력

--- CombinedCode.py
+++ CombinedCode.py
@@ -1,4 +1,4 @@
-#통합시작
+#이미 추천된 옷 더이상 수정하지 않도록 수정 보완 성공하였습니다.
 import pandas as pd
 from difflib import get_close_matches
 import random
@@ -13,7 +13,6 @@ except FileNotFoundError:
 # 사용자로부터 정보 입력 받기
 current_situation = input("어떤 상황에서 입을 옷을 추천받고 싶으세요?: ")
 current_color = input("어떤 색깔의 옷을 추천받고 싶으세요?: ")
-last_outfit = ""
 
 # 유사한 단어 찾기 함수 정의
 def get_similar_words(word, words_list):
@@ -21,29 +20,36 @@ def get_similar_words(word, words_list):
     words_list = [str(w) for w in words_list]
     return get_close_matches(word, words_list, n=1, cutoff=0.8)
 
+# 추천된 코디를 저장하는 리스트
+recommended_outfits = []
 
 while True:
     # 입력받은 정보와 유사한 데이터 추출
     filtered_df = df[df['codi'].str.contains('|'.join(get_similar_words(current_situation, df['codi'])), na=False)]
     filtered_df = filtered_df[df['color'].str.contains('|'.join(get_similar_words(current_color, df['color'])), na=False)]
-    
-    # 필터링된 추천 받을 데이터를 배열에 저장
-    recommended_outfits = filtered_df['title'].tolist()
 
-    # 추천할 옷 랜덤 선택
-    if recommended_outfits:
-        recommended_outfits = [outfit for outfit in recommended_outfits if outfit != last_outfit]  # 이전에 추천된 코디 제외
-        if recommended_outfits:
-            random_outfit = random.choice(recommended_outfits)
-            print("추천되는 코디:")
-            print(random_outfit)
-            last_outfit = random_outfit  # 선택된 코디 저장
-        else:
-            print("일치하는 코디가 없습니다.")
-            break
-    else:
+    # 추천할 옷 선택
+    if filtered_df.empty:
         print("일치하는 코디가 없습니다.")
         break
+    else:
+        # 추천 데이터에서 중복되지 않게 선택하기 위해
+        # 추천된 코디 리스트와 비교하여 중복되지 않는 코디만 추천 리스트에 추가
+        unique_outfits = []
+        for outfit in filtered_df['title'].tolist():
+            if outfit not in recommended_outfits:
+                unique_outfits.append(outfit)
+
+        # 중복되지 않는 코디가 있다면 추천 리스트에서 랜덤으로 선택하고, 기존 추천 리스트에 추가
+        if unique_outfits:
+            random_outfit = random.choice(unique_outfits)
+            recommended_outfits.append(random_outfit)
+            print("추천되는 코디:")
+            print(random_outfit)
+        # 중복되지 않는 코디가 없다면 모든 코디가 추천 리스트에 이미 추가되었다는 뜻이므로 종료
+        else:
+            print("더 이상 추천할 코디가 없습니다.")
+            break
 
     # 사용자의 답변 받기
     answer = input("추천된 코디가 마음에 드나요? (예/아니오): ")
