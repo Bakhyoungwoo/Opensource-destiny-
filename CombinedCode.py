0d0ac50f89ee6a7d763a175a4ab18babe80d8d24
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-05 12:14:22 +0900

    Create CombinedCode.py

---
+++ CombinedCode.py
@@ -0,0 +1,50 @@
+#통합시작
+import pandas as pd
+from difflib import get_close_matches
+import random
+
+try:
+    # 엑셀 파일 로드
+    df = pd.read_excel('codi_data.xlsx')
+except FileNotFoundError:
+    print("열고자 하는 엑셀 파일이 존재하지 않습니다.")
+    exit()
+
+# 사용자로부터 상황 및 색깔 입력 받기
+current_situation = input("어떤 상황에서 입을 옷을 추천받고 싶으세요?: ")
+current_color = input("어떤 색깔의 옷을 추천받고 싶으세요?: ")
+
+# 유사한 단어 찾기 함수 정의
+def get_similar_words(word, words_list):
+    # 문자열 타입의 값만 words_list에 추가
+    words_list = [str(w) for w in words_list]
+    return get_close_matches(word, words_list, n=1, cutoff=0.8)
+
+
+while True:
+    # 입력받은 정보와 유사한 데이터 추출
+    filtered_df = df[df['codi'].str.contains('|'.join(get_similar_words(current_situation, df['codi'])), na=False)]
+    filtered_df = filtered_df[df['color'].str.contains('|'.join(get_similar_words(current_color, df['color'])), na=False)]
+    
+    # 필터링된 추천 받을 데이터를 배열에 저장
+    recommended_outfits = filtered_df['title'].tolist()
+
+    # 추천할 옷 랜덤 선택
+    if recommended_outfits:
+        random_outfit = random.choice(recommended_outfits)
+        print("추천되는 코디:")
+        print(random_outfit)
+    else:
+        print("일치하는 코디가 없습니다.")
+        break
+
+    # 사용자의 답변 받기
+    answer = input("추천된 코디가 마음에 드나요? (예/아니오): ")
+    if answer.lower() == '예':
+        print("좋아요! 코디가 마음에 드셨다니 다행입니다!")
+        break
+    elif answer.lower() == '아니오':
+        print("다른 코디를 추천해드릴게요!")
+        continue
+    else:
+        print("예 또는 아니오로 대답해주세요.")
