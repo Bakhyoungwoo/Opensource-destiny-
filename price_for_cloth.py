359edd4eae2ef1007b3fa8d27f2affb52306ac13
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-13 21:24:54 +0900

    옷에 대한 모든 가격 제공 뼈대
    만들어야 할 내용
    
    1. 사용자가 옷 이름을 정확하게 입력
     
     2.  해당 옷의 여러 가격들을 추천해줌. (같은 이름의 여러 data가 갖는 각각의 가격들을 나타나게 함.)
     
    3.  거기에 추가로 비슷한 가격, 비슷한 색깔을 사용자에게 추가로 제공하는 방식

---
+++ price_for_cloth.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# 사용자로부터 옷 이름을 입력받음
+cloth_name = input("옷 이름을 입력하세요: ")
+
+# 엑셀 파일을 불러옴
+df = pd.read_excel('example.xlsx')
+
+# title열에서 옷 이름이 일치하는 행을 찾음
+matching_rows = df.loc[df['title'] == cloth_name]
+
+# 가격 정보를 담을 리스트를 생성
+prices = []
+
+# 찾은 행에서 가격 정보를 추출하여 리스트에 추가
+for index, row in matching_rows.iterrows():
+    prices.append(row['price'])
+
+# 가격 정보를 출력
+if len(prices) == 0:
+    print("해당하는 옷 이름이 없습니다.")
+else:
+    print(f"{cloth_name}의 가격은 {prices}원 입니다.")
