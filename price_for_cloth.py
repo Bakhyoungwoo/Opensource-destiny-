3085343b0215c656122a16630e846fc7f2311d40
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-15 17:02:11 +0900

    오류 수정
    matching_rows['color'].tolist()를 사용하여 matching_rows 데이터프레임의 'color' 열을 리스트로 변환.
    이 리스트에는 입력한 옷과 같은 색깔 정보가 포함됨.
    
    df['color'].isin(cloth_color_list)를 사용하여 데이터프레임의 'color' 열이 입력한 옷과 같은 색깔 리스트에 속하는 경우를 선택.
    이를 통해 색깔이 같은 옷만 추출함.
    
    추출된 색깔이 같은 옷을 순회하면서, 각 옷의 'title' 값을 확인함.
     
    만약 옷의 'title' 값이 입력한 옷의 이름과 같지 않은 경우에만 recommend_clothes_same_color 리스트에 해당 옷의 'title' 값을 추가하여 입력한 옷 자체는 추천에서 제외토록 함.
    
    recommend_clothes_same_color 리스트에는 입력한 옷과 같은 색깔을 가진 다른 옷의 이름이 포함됨.

--- price_for_cloth.py
+++ price_for_cloth.py
@@ -43,16 +43,15 @@ else:
     else:
         print("추천할 비슷한 가격대의 옷이 없습니다.")
         
-    # 입력한 옷과 같은 색의 옷 추천
-    cloth_color = matching_rows.iloc[0]['color']  # 입력한 옷과 같은 색깔 정보
-    same_color_clothes = df.loc[df['color'] == cloth_color]  # 색깔이 같은 옷 추출
+   # 입력한 옷과 같은 색의 옷 추천
+    cloth_color_list = matching_rows['color'].tolist()  # 입력한 옷과 같은 색깔 정보 리스트로 변환
+    same_color_clothes = df.loc[df['color'].isin(cloth_color_list)]  # 색깔이 같은 옷 추출
 
-    #같은 색깔의 옷 추천 리스트 생성
     recommend_clothes_same_color = []
     for _, row in same_color_clothes.iterrows():
         if row['title'] != cloth_name:
             recommend_clothes_same_color.append(row['title'])
-            
+
     # 추천할 옷이 있다면 출력
     if len(recommend_clothes_same_color) > 0:
         print(f"입력한 옷과 같은 색의 옷 추천: {recommend_clothes_same_color}")
