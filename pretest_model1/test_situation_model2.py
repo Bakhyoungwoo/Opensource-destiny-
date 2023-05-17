8976e84fd11342a45c3cc38f65fdfdb4722c92b1
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2023-05-17 22:55:23 +0900

    modify_6

--- pretest_model1/test_situation_model2.py
+++ pretest_model1/test_situation_model2.py
@@ -34,7 +34,7 @@ def create_situation_window():
     label_sex.pack()
     situation_sex = Entry(app, width = 20)
     situation_sex.pack()
-    situation_sex.insert(0, "ex : man or woman")
+    situation_sex.insert(0, "ex : 남성 or 여성")
     
     # current color 변수에 색깔 값 저장
     def btnsave_situation_sex():
@@ -51,7 +51,7 @@ def create_situation_window():
     label_situation.pack()
     situation = Entry(app, width = 20)
     situation.pack()
-    situation.insert(0, "ex : wedding")
+    situation.insert(0, "ex : 결혼식")
  
     def btnsave_situation():
         #전역변수 설정
@@ -66,7 +66,7 @@ def create_situation_window():
     label_color.pack()
     situation_color = Entry(app, width = 20)
     situation_color.pack()
-    situation_color.insert(0, "ex : blue")
+    situation_color.insert(0, "ex : 파란색")
     
     # current color 변수에 색깔 값 저장
     def btnsave_situation_color():
@@ -78,8 +78,6 @@ def create_situation_window():
     btn_color = Button(app, text="click", command=btnsave_situation_color)
     btn_color.place(x=900,y=100)
     
-        # 엑셀 파일 로드
-    df = pd.read_excel('codi_data.xlsx')
     # 유사한 단어 찾기 함수 정의
     def get_similar_words(word, words_list):
         # 문자열 타입의 값만 words_list에 추가
@@ -90,43 +88,37 @@ def create_situation_window():
     recommended_outfits = []
 
     while True:
-        # 입력받은 정보와 유사한 데이터 추출
-        filtered_df = df[df['codi'].str.contains('|'.join(get_similar_words(current_situation, df['codi'])), na=False)]
-        filtered_df = filtered_df[df['color'].str.contains('|'.join(get_similar_words(current_color, df['color'])), na=False)]
+    # 입력받은 정보와 유사한 데이터 추출
+        filtered_df = df[df['gender'].str.contains('|'.join(get_similar_words(current_sex, df['gender'])), na=False)]
+        filtered_df = filtered_df[df['codi'].str.contains('|'.join(get_similar_words(current_situation, df['codi'])), na=False)]
+        filtered_df = filtered_df[filtered_df['color'].str.contains('|'.join(get_similar_words(current_color, df['color'])), na=False)]
 
-        # 추천할 옷 선택
+    # 추천할 옷 선택
         if filtered_df.empty:
-            label_empty=Label(app,text="일치하는 코디가 없습니다.")
-            break
+          label_empty = Label(app,text="일치하는 코디가 없습니다.")
+          label_empty.pack()
+          break
         else:
-            # 추천 데이터에서 중복되지 않게 선택하기 위해
-            # 추천된 코디 리스트와 비교하여 중복되지 않는 코디만 추천 리스트에 추가
+        # 추천 데이터에서 중복되지 않게 선택하기 위해
+        # 추천된 코디 리스트와 비교하여 중복되지 않는 코디만 추천 리스트에 추가
             unique_outfits = []
+            
             for outfit in filtered_df['title'].tolist():
                 if outfit not in recommended_outfits:
                     unique_outfits.append(outfit)
 
-        # 중복되지 않는 코디가 있다면 추천 리스트에서 랜덤으로 선택하고, 기존 추천 리스트에 추가
+            # 중복되지 않는 코디가 있다면 추천 리스트에서 랜덤으로 선택하고, 기존 추천 리스트에 추가
             if unique_outfits:
                 random_outfit = random.choice(unique_outfits)
                 recommended_outfits.append(random_outfit)
-                print("추천되는 코디:")
+                label_recommend = Label(app, text = "추천되는 코디:")
+                label_recommend.pack()
                 print(random_outfit)
-            # 중복되지 않는 코디가 없다면 모든 코디가 추천 리스트에 이미 추가되었다는 뜻이므로 종료
+        # 중복되지 않는 코디가 없다면 모든 코디가 추천 리스트에 이미 추가되었다는 뜻이므로 종료
             else:
                 print("더 이상 추천할 코디가 없습니다.")
                 break
 
-    # 사용자의 답변 받기
-        answer = input("추천된 코디가 마음에 드나요? (예/아니오): ")
-        if answer.lower() == '예':
-            print("좋아요! 코디가 마음에 드셨다니 다행입니다!")
-            break
-        elif answer.lower() == '아니오':
-            print("다른 코디를 추천해드릴게요!")
-            continue
-        else:
-            print("예 또는 아니오로 대답해주세요.")
     
     app.mainloop()
     
