5345b2c972f6eae93dc13b779882e81fc7ded7ea
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-20 17:12:05 +0900

    딕셔너리를 이용한 색조합 피드백 틀

--- ColorCombination.py
+++ ColorCombination.py
@@ -18,6 +18,7 @@ if len(shirt_color) > 0:
     print(f"입력하신 상의 '{shirt_name}'의 색상: {shirt_color}")
 else:
     print(f"입력하신 상의 '{shirt_name}'에 대한 정보를 찾을 수 없습니다.")
+    exit()
 
 # 사용자로부터 하의 이름 입력 받기
 pants_name = input("찾고 계신 하의 이름을 입력하세요: ")
@@ -29,3 +30,13 @@ if len(pants_color) > 0:
     print(f"입력하신 하의 '{pants_name}'의 색상: {pants_color}")
 else:
     print(f"입력하신 하의 '{pants_name}'에 대한 정보를 찾을 수 없습니다.")
+    exit()
+
+#색상 조합 정보를 담은 딕셔너리 생성
+color_combinations = {
+}
+
+if shirt_color in color_combinations and pants_color in color_combinations[shirt_color]:
+     print(f"상의와 하의의 색상 조합이 잘 어울립니다! ({shirt_color}, {pants_color})")
+else:
+     print(f"상의와 하의의 색상 조합이 잘 어울리지 않습니다. 다른 조합을 고려해 보세요!")
