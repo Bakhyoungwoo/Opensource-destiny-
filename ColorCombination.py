35f715b658061c0418d6dddf212597c7b38e36ab
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-20 16:16:37 +0900

    예외처리  추가

--- ColorCombination.py
+++ ColorCombination.py
@@ -9,16 +9,23 @@ pants_file = 'pants.xlsx'
 pants_data = pd.read_excel(pants_file)
 
 # 사용자로부터 상의 이름 입력 받기
-shirt_name = input("찾고계신 상의 이름을 입력하세요: ")
-
-# 사용자로부터 하의 이름 입력 받기
-pants_name = input("찾고계신 하의 이름을 입력하세요: ")
+shirt_name = input("찾고 계신 상의 이름을 입력하세요: ")
 
 # shirts 엑셀 파일에서 상의 색상 추출
-shirt_color = shirts_data.loc[shirts_data['title'] == shirt_name, 'color'].values[0]
+shirt_color = shirts_data.loc[shirts_data['title'] == shirt_name, 'color']
+if len(shirt_color) > 0:
+    shirt_color = shirt_color.values[0]
+    print(f"입력하신 상의 '{shirt_name}'의 색상: {shirt_color}")
+else:
+    print(f"입력하신 상의 '{shirt_name}'에 대한 정보를 찾을 수 없습니다.")
+
+# 사용자로부터 하의 이름 입력 받기
+pants_name = input("찾고 계신 하의 이름을 입력하세요: ")
 
 # pants 엑셀 파일에서 하의 색상 추출
-pants_color = pants_data.loc[pants_data['title'] == pants_name, 'color'].values[0]
-
-print(f"입력하신 상의 '{shirt_name}'의 색상: {shirt_color}")
-print(f"입력하신 하의 '{pants_name}'의 색상: {pants_color}")
+pants_color = pants_data.loc[pants_data['title'] == pants_name, 'color']
+if len(pants_color) > 0:
+    pants_color = pants_color.values[0]
+    print(f"입력하신 하의 '{pants_name}'의 색상: {pants_color}")
+else:
+    print(f"입력하신 하의 '{pants_name}'에 대한 정보를 찾을 수 없습니다.")
