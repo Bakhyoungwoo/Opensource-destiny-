f4b0281dbfaa66ead9907f15a88907ad033f3a39
wan-su-2020 <poj5928@gmail.com>
2023-05-28 19:13:04 +0900

    title 출력

--- crawling_musinsa_shirts.py
+++ crawling_musinsa_shirts.py
@@ -8,3 +8,16 @@ url = 'https://www.musinsa.com/categories/item/001' #해당 URL갖고옴
 driver = webdriver.Chrome()
 driver.get(url)
 driver.implicitly_wait(3)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+title_list=[] #타이틀을 저장할 배열 생성
+items = soup.find('ul', id='searchList').find_all('li', class_='li_box') 
+for i, item in enumerate(items):
+    title = item.find('div', class_='article_info').find('p', class_='list_info').find('a').get_text(strip=True)#상의 타이틀
+    
+    title_list.append(title)
+    
+    
+print(title) #콘솔창에 타이틀 출력
+
