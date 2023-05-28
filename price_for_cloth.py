3ed265cafb8756598e9a7d6f26134debec4634ab
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-05-28 10:44:40 +0900

    similar_cloth_name() 변수 관련 정의와 수정 사항

--- price_for_cloth.py
+++ price_for_cloth.py
@@ -21,8 +21,11 @@ def find_similar_cloth_name(name, names_list):
             similar_name = n
     return similar_name
 
+# 가장 유사한 옷 이름을 찾음
+similar_cloth_name = find_similar_cloth_name(cloth_name, df['title'].tolist())
+
 # title열에서 옷 이름이 일치하는 행을 찾음
-matching_rows = df.loc[df['title'].str.lower() == cloth_name_lower]
+matching_rows = df.loc[df['title'].str.lower() == similar_cloth_name.lower()]
 
 # 가격 정보를 담을 리스트를 생성
 prices = []
@@ -39,20 +42,20 @@ if len(prices) == 0:
 else:
     average_price = sum(prices) / len(prices)
     min_price = min(prices)
-    print(f"{cloth_name}의 가격은 {prices}원이며, 평균 가격은 {average_price:,}원, 가장 싼 가격은 {min_price:,}원 입니다.") #가독성을 위해 가격 출력 시 : ,를 사용하여 천 단위로 쉼표를 출력
+    print(f"{similar_cloth_name}의 가격은 {prices}원이며, 평균 가격은 {average_price:,}원, 가장 싼 가격은 {min_price:,}원 입니다.") #가독성을 위해 가격 출력 시 : ,를 사용하여 천 단위로 쉼표를 출력
 
-     # 평균 가격의 오차 범위 0.1% price_range 변수에 저장
+    # 평균 가격의 오차 범위 0.1% price_range 변수에 저장
     price_range = average_price * 0.1
     
-     # 추천할 옷의 이름을 저장할 리스트 생성 
+    # 추천할 옷의 이름을 저장할 리스트 생성 
     recommend_clothes_similar_price = []
     for index, row in df.iterrows():
         price_str = row['price']
         price_num = int(price_str.replace('원', '').replace(',', ''))
-    if abs(price_num - average_price) <= price_range and row['title'].lower() != cloth_name_lower:
-           # 불필요한 정보인 [무료반품]을 제거하여 추천할 옷의 이름을 저장
-        clean_title = row['title'].replace('[무료반품]', '').strip()
-        recommend_clothes_similar_price.append(clean_title)
+        if abs(price_num - average_price) <= price_range and row['title'].lower() != similar_cloth_name.lower():
+            # 불필요한 정보인 [무료반품]을 제거하여 추천할 옷의 이름을 저장
+            clean_title = row['title'].replace('[무료반품]', '').strip()
+            recommend_clothes_similar_price.append(clean_title)
     
     # 추천할 옷이 있다면 출력
     if len(recommend_clothes_similar_price) > 0:
@@ -60,18 +63,17 @@ else:
     else:
         print("추천할 비슷한 가격대의 옷이 없습니다.")
         
-  
     # 입력한 옷과 같은 색의 옷 추천
     cloth_colors = matching_rows['color'].str.lower().tolist()  # 입력한 옷과 같은 색깔 정보 리스트로 변환
     if len(cloth_colors) > 0:
-        same_color_clothes = df.loc[(df['title'].str.lower() != cloth_name_lower) & (df['color'].str.lower().isin(cloth_colors))]  # 색깔이 같은 옷 추출
+        same_color_clothes = df.loc[(df['title'].str.lower() != similar_cloth_name.lower()) & (df['color'].str.lower().isin(cloth_colors))]  # 색깔이 같은 옷 추출
 
         recommend_clothes_same_color = same_color_clothes['title'].tolist()
 
-         # 불필요한 정보를 제거하여 추천할 옷의 이름을 저장
+        # 불필요한 정보를 제거하여 추천할 옷의 이름을 저장
         recommend_clothes_same_color = [title.replace('[무료반품]', '').strip() for title in recommend_clothes_same_color]
 
-    # 추천할 옷이 있다면 출력
+        # 추천할 옷이 있다면 출력
         if len(recommend_clothes_same_color) > 0:
             print(f"입력한 옷과 같은 색의 옷 추천: {recommend_clothes_same_color}")
         else:
