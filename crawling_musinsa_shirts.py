1d4aa281309dfc82aa4d8cf4a46ba356f3ee1ab4
wan-su-2020 <poj5928@gmail.com>
2023-05-28 19:20:00 +0900

    가격 추가

--- crawling_musinsa_shirts.py
+++ crawling_musinsa_shirts.py
@@ -12,12 +12,16 @@ driver.implicitly_wait(3)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 title_list=[] #타이틀을 저장할 배열 생성
+price_list=[] #가격을 저장할 배열 생성
 items = soup.find('ul', id='searchList').find_all('li', class_='li_box') 
 for i, item in enumerate(items):
     title = item.find('div', class_='article_info').find('p', class_='list_info').find('a').get_text(strip=True)#상의 타이틀
-    
+    price_tag = item.find('div', class_='article_info').find('p', class_='price')#하의 가격
+    price = price_tag.get_text(strip=True) #가격 추출
     title_list.append(title)
+    price_list.append(price)
     
     
 print(title) #콘솔창에 타이틀 출력
+print(price) #콘솔창에 가격 출력
 
