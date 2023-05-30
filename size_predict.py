28a98b7e75b063b2ec8f84dcd61e3d65b928131f
wan-su-2020 <poj5928@gmail.com>
2023-05-30 14:16:09 +0900

    대략적인 틀 구현

--- size_predict.py
+++ size_predict.py
@@ -9,3 +9,12 @@ y = np.array([1, 2, 3, 4, 5])  # 출력 데이터 (옷 사이즈)
 model = LinearRegression()
 model.fit(X, y)
 
+# 사용자로부터 키와 몸무게 입력 받기
+height = float(input("키를 입력하세요 (cm): "))
+weight = float(input("몸무게를 입력하세요 (kg): "))
+
+# 입력 데이터로 예측하기
+size = model.predict([[height, weight]])
+
+# 결과 출력
+print(f"예상 옷 사이즈는 {size}입니다.")
