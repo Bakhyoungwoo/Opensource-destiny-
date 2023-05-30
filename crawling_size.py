1f4a8a97746132849e1146e41b076e2cddc9816e
wan-su-2020 <poj5928@gmail.com>
2023-05-30 14:38:27 +0900

    옷 사이즈+ 구매자 키, 몸무게

--- crawling_size.py
+++ crawling_size.py
@@ -12,13 +12,16 @@ driver.implicitly_wait(3)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 body_list = []
-
+size_list = []
 items = soup.find('div', class_='review-list-wrap').find_all('div', class_='review-list')
 for item in items:
     body = item.find('div', class_='review-profile__text-wrap').find('div', class_="review-profile__information").find('p').get_text(strip=True)
-    
+    size = item.find('div', class_='review-goods-information').find('div', class_='review-goods-information__item').find('p').get_text(strip=True)
     body_list.append(body)
-
+    size_list.append(size)
+    
     print(body)
-   
+    print(size)
+    
+
 
