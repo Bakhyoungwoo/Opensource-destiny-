6706375f860c91d643e8928e90118131855af226
wan-su-2020 <poj5928@gmail.com>
2023-05-30 14:36:21 +0900

    Update crawling_size.py

--- crawling_size.py
+++ crawling_size.py
@@ -11,3 +11,14 @@ driver.implicitly_wait(3)
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+body_list = []
+
+items = soup.find('div', class_='review-list-wrap').find_all('div', class_='review-list')
+for item in items:
+    body = item.find('div', class_='review-profile__text-wrap').find('div', class_="review-profile__information").find('p').get_text(strip=True)
+    
+    body_list.append(body)
+
+    print(body)
+   
+
