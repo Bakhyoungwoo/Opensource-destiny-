d2d97f4658229be1d5d4d0c2885223089601cfcb
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-06-01 16:27:51 +0900

    필요 모듈 import 완료

--- project_situation/z_color_file.py
+++ project_situation/z_color_file.py
@@ -1,3 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import pandas as pd
+from PyQt5 import uic
