430909b2c2d94f30270cc7c8da0b06dfb6dc8c41
U-Seok, Kang <73337570+MuHaRVEY@users.noreply.github.com>
2023-06-01 16:29:59 +0900

    Color_FirstWindow 클래스 정의 시작

--- project_situation/z_color_file.py
+++ project_situation/z_color_file.py
@@ -5,3 +5,11 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import pandas as pd
 from PyQt5 import uic
+
+form_class1 = uic.loadUiType("first_window.ui")[0]
+
+class Color_FirstWindow(QMainWindow, form_class1):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setupUi(self)
+        self.pushButton_color.clicked.connect(self.button_click_color)
